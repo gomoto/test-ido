2a7b7b5453c67e67e9f8d2dbc643802dd22ab050
Ryan Gomoto <ryan@cyrusbio.com>
2017-05-04 21:55:46 -0700

    images

--- scripts/images.js
+++ scripts/images.js
@@ -1,8 +1,7 @@
 const image = require('ido/image')
 
 image.copy('./images/**/*.jpg', './build/images', {
-  // relative to ./build/images
-  manifest: 'image-manifest.json'
+  manifest: './build/images/image-manifest.json'
 })
 .then(() => {
   console.log('images done')
